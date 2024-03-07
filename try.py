744a7c5357c9228f84090f5e0ff33301aa32af85
lusca04 <rodriguwalusca@gmail.com>
2024-03-06 22:08:05 -0300

    calculadora

--- try.py
+++ try.py
@@ -1,20 +1,97 @@
 print('Bem Vindo a Calculadora Luquinhas')
 
 escolha = input("Escolha (soma/soma3/menos/menos3/mult/mult3/div):")
+Quantidade_de_Números = input("Quantidade de números que vai ser utilizado (2/3/4/5/6/7/8/9/10):")
 
-
-if escolha == "soma":
+if escolha == "soma" and Quantidade_de_Números == "2":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
-    soma = n1 + n2
-    print("O Resultado da soma com dois valores é:", soma)
+    soma2 = n1 + n2
+    print("O Resultado da soma com dois valores é:", soma2)
 
-if escolha == "soma3":
+if escolha == "soma" and Quantidade_de_Números == "3":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
     n3 = float(input("Valor 3:"))
     soma3 = n1 + n2 + n3
-    print("O Resultado da soma com três valores é:", soma3)
+    print("O Resultado da soma com dois valores é:", soma3)
+
+if escolha == "soma" and Quantidade_de_Números == "4":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    soma4 = n1 + n2 + n3 + n4
+    print("O Resultado da soma com dois valores é:", soma4)
+
+if escolha == "soma" and Quantidade_de_Números == "5":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    soma5 = n1 + n2 + n3 + n4 + n5
+    print("O Resultado da soma com dois valores é:", soma5)
+
+if escolha == "soma" and Quantidade_de_Números == "6":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    soma6 = n1 + n2 + n3 + n4 + n5 +n6
+    print("O Resultado da soma com dois valores é:", soma6)
+
+if escolha == "soma" and Quantidade_de_Números == "7":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    soma7 = n1 + n2 + n3 + n4 + n5 + n6 + n7
+    print("O Resultado da soma com dois valores é:", soma7)
+
+if escolha == "soma" and Quantidade_de_Números == "8":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    soma8 = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8
+    print("O Resultado da soma com dois valores é:", soma8)
+
+if escolha == "soma" and Quantidade_de_Números == "9":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    n9 = float(input("Valor 9:"))
+    soma9 = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9
+    print("O Resultado da soma com dois valores é:", soma9)
+
+if escolha == "soma" and Quantidade_de_Números == "10":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    n9 = float(input("Valor 9:"))
+    n10 = float(input("Valor 10:"))
+    soma10 = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10
+    print("O Resultado da soma com dois valores é:", soma10)
 
 if escolha == "menos":
     n1 = float(input("Valor 1:"))
