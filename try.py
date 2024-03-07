e4481b95d3f9a887443c94b4ef87b4352be10506
lusca04 <rodriguwalusca@gmail.com>
2024-03-06 22:29:48 -0300

    calculadora

--- try.py
+++ try.py
@@ -8,14 +8,12 @@ if escolha == "soma" and Quantidade_de_Números == "2":
     n2 = float(input("Valor 2:"))
     soma2 = n1 + n2
     print("O Resultado da soma com dois valores é:", soma2)
-
 if escolha == "soma" and Quantidade_de_Números == "3":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
     n3 = float(input("Valor 3:"))
     soma3 = n1 + n2 + n3
     print("O Resultado da soma com dois valores é:", soma3)
-
 if escolha == "soma" and Quantidade_de_Números == "4":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
@@ -23,7 +21,6 @@ if escolha == "soma" and Quantidade_de_Números == "4":
     n4 = float(input("Valor 4:"))
     soma4 = n1 + n2 + n3 + n4
     print("O Resultado da soma com dois valores é:", soma4)
-
 if escolha == "soma" and Quantidade_de_Números == "5":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
@@ -32,7 +29,6 @@ if escolha == "soma" and Quantidade_de_Números == "5":
     n5 = float(input("Valor 5:"))
     soma5 = n1 + n2 + n3 + n4 + n5
     print("O Resultado da soma com dois valores é:", soma5)
-
 if escolha == "soma" and Quantidade_de_Números == "6":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
@@ -40,9 +36,8 @@ if escolha == "soma" and Quantidade_de_Números == "6":
     n4 = float(input("Valor 4:"))
     n5 = float(input("Valor 5:"))
     n6 = float(input("Valor 6:"))
-    soma6 = n1 + n2 + n3 + n4 + n5 +n6
+    soma6 = n1 + n2 + n3 + n4 + n5 + n6
     print("O Resultado da soma com dois valores é:", soma6)
-
 if escolha == "soma" and Quantidade_de_Números == "7":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
@@ -53,7 +48,6 @@ if escolha == "soma" and Quantidade_de_Números == "7":
     n7 = float(input("Valor 7:"))
     soma7 = n1 + n2 + n3 + n4 + n5 + n6 + n7
     print("O Resultado da soma com dois valores é:", soma7)
-
 if escolha == "soma" and Quantidade_de_Números == "8":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
@@ -65,7 +59,6 @@ if escolha == "soma" and Quantidade_de_Números == "8":
     n8 = float(input("Valor 8:"))
     soma8 = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8
     print("O Resultado da soma com dois valores é:", soma8)
-
 if escolha == "soma" and Quantidade_de_Números == "9":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
@@ -78,7 +71,6 @@ if escolha == "soma" and Quantidade_de_Números == "9":
     n9 = float(input("Valor 9:"))
     soma9 = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9
     print("O Resultado da soma com dois valores é:", soma9)
-
 if escolha == "soma" and Quantidade_de_Números == "10":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
@@ -92,33 +84,167 @@ if escolha == "soma" and Quantidade_de_Números == "10":
     n10 = float(input("Valor 10:"))
     soma10 = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10
     print("O Resultado da soma com dois valores é:", soma10)
-
-if escolha == "menos":
+if escolha == "menos" and Quantidade_de_Números == "2":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
-    menos = n1 - n2
-    print("O Resultado da subtração com dois valores é:", menos)
-
-if escolha == "menos3":
+    menos2 = n1 - n2
+    print("O Resultado da subtração com dois valores é:", menos2)
+if escolha == "menos" and Quantidade_de_Números == "3":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
     n3 = float(input("Valor 3:"))
     menos3 = n1 - n2 - n3
     print("O Resultado da subtração com três valores é:", menos3)
-
-if escolha == "mult":
+if escolha == "menos" and Quantidade_de_Números == "4":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
-    mult = n1 * n2
-    print("O Resultado da multiplicação com dois valores é:", mult)
-
-if escolha == "mult3":
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    menos4 = n1 - n2 - n3 - n4
+    print("O Resultado da soma com dois valores é:", menos4)
+if escolha == "menos" and Quantidade_de_Números == "5":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    menos5 = n1 - n2 - n3 - n4 - n5
+    print("O Resultado da soma com dois valores é:", menos5)
+if escolha == "menos" and Quantidade_de_Números == "6":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    menos6 = n1 - n2 - n3 - n4 - n5 - n6
+    print("O Resultado da soma com dois valores é:", menos6)
+if escolha == "menos" and Quantidade_de_Números == "7":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    menos7 = n1 - n2 - n3 - n4 - n5 - n6 - n7
+    print("O Resultado da soma com dois valores é:", menos7)
+if escolha == "menos" and Quantidade_de_Números == "8":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    menos8 = n1 - n2 - n3 - n4 - n5 - n6 - n7 - n8
+    print("O Resultado da soma com dois valores é:", menos8)
+if escolha == "menos" and Quantidade_de_Números == "9":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    n9 = float(input("Valor 9:"))
+    menos9 = n1 - n2 - n3 - n4 - n5 - n6 - n7 - n8 - n9
+    print("O Resultado da soma com dois valores é:", menos9)
+if escolha == "menos" and Quantidade_de_Números == "10":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    n9 = float(input("Valor 9:"))
+    n10 = float(input("Valor 10:"))
+    menos10 = n1 - n2 - n3 - n4 - n5 - n6 - n7 - n8 - n9 - n10
+    print("O Resultado da soma com dois valores é:", menos10)
+if escolha == "mult" and Quantidade_de_Números == "2":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    mult2 = n1 * n2
+    print("O Resultado da multiplicação com dois valores é:", mult2)
+if escolha == "mult" and Quantidade_de_Números == "3":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
     n3 = float(input("Valor 3:"))
     mult3 = n1 * n2 * n3
-    print("O Resultado da multiplicação com três valores é:", mult3)
-
+    print("O Resultado da multiplicação com dois valores é:", mult3)
+if escolha == "mult" and Quantidade_de_Números == "4":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    mult4 = n1 * n2 * n3 * n4
+    print("O Resultado da multiplicação com dois valores é:", mult4)
+if escolha == "mult" and Quantidade_de_Números == "5":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    mult5 = n1 * n2 * n3 * n4 * n5
+    print("O Resultado da multiplicação com dois valores é:", mult5)
+if escolha == "mult" and Quantidade_de_Números == "6":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    mult6 = n1 * n2 * n3 * n4 * n5 * n6
+    print("O Resultado da multiplicação com dois valores é:", mult6)
+if escolha == "mult" and Quantidade_de_Números == "7":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    mult7 = n1 * n2 * n3 * n4 * n5 * n6 * n7
+    print("O Resultado da multiplicação com dois valores é:", mult7)
+if escolha == "mult" and Quantidade_de_Números == "8":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    mult8 = n1 * n2 * n3 * n4 * n5 * n6 * n7 * n8
+    print("O Resultado da multiplicação com dois valores é:", mult8)
+if escolha == "mult" and Quantidade_de_Números == "9":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    n9 = float(input("Valor 9:"))
+    mult9 = n1 * n2 * n3 * n4 * n5 * n6 * n7 * n8 * n9
+    print("O Resultado da multiplicação com dois valores é:", mult9)
+if escolha == "mult" and Quantidade_de_Números == "10":
+    n1 = float(input("Valor 1:"))
+    n2 = float(input("Valor 2:"))
+    n3 = float(input("Valor 3:"))
+    n4 = float(input("Valor 4:"))
+    n5 = float(input("Valor 5:"))
+    n6 = float(input("Valor 6:"))
+    n7 = float(input("Valor 7:"))
+    n8 = float(input("Valor 8:"))
+    n9 = float(input("Valor 9:"))
+    n10 = float(input("Valor 10:"))
+    mult10 = n1 * n2 * n3 * n4 * n5 * n6 * n7 * n8 * n9 * n10
+    print("O Resultado da multiplicação com dois valores é:", mult10)
 if escolha == "d":
     n1 = float(input("Valor 1:"))
     n2 = float(input("Valor 2:"))
