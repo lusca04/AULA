c6f9164292a14f24d8d8b7b863b6d0d123af9a99
lusca04 <162514161+lusca04@users.noreply.github.com>
2024-05-22 17:19:38 -0300

    Delete sss.py

--- sss.py
+++
@@ -1,8 +0,0 @@
-dep = float(input("Digite a quantia a ser depositada: "))
-tax = float(input("Quantia de juros: "))
-mes = 1
-while mes <= 24:
-dep= dep+(dep*(tax/100))
-print(f"{mes} é de R${dep:.2f}.")
-mes= mes+1
-print(f"O Ganho é de R${dep:.2f}.")
